51df43a7526adad9e26acc41186746747030e4e1
Buovsky <57236090+Buovsky@users.noreply.github.com>
2020-04-25 17:20:28 +0200

    Initial commit

---
+++ AirMonitor/AirMonitor/Views/MainPage.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace AirMonitor.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Help_Clicked(object sender, EventArgs e)
+        {
+            DisplayAlert("Tytuł dialogu", "elo", "elo");
+            DisplayActionSheet("Tytul", "Anuluj", "destruction", "jeden", "dwa", "trzy");
+        }
+
+    }
+}
