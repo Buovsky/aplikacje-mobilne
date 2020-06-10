9606afb26d1771d7be14188dbfeca4ed10df280b
Buovsky <57236090+Buovsky@users.noreply.github.com>
2020-06-10 19:31:09 +0200

    Linked ListView elements to DetailsPage

--- AirMonitor/AirMonitor/Views/HomePage.xaml.cs
+++ AirMonitor/AirMonitor/Views/HomePage.xaml.cs
@@ -8,19 +8,26 @@ using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 using AirMonitor.ViewModels;
 using Xamarin.Essentials;
-
+using AirMonitor.Models;
 
 namespace AirMonitor.Views
 {
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class HomePage : ContentPage
     {
+        private HomeViewModel _viewModel => BindingContext as HomeViewModel;
+
         public HomePage()
         {
             InitializeComponent();
 
             BindingContext = new HomeViewModel(Navigation);
 
+        }
+
+        void ListView_ItemTapped(System.Object sender, Xamarin.Forms.ItemTappedEventArgs e)
+        {
+            _viewModel.MoveToDetailsCommand.Execute(e.Item as Measurement);
         }
     }
 }
